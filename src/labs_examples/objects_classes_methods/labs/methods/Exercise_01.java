e851218021325f1bcfe3531c9ca6b1b4086836b7
sarahschoonmaker <8761813+SarahSchoonmaker@users.noreply.github.com>
2023-05-06 14:39:19 -0400

    methods labs

--- src/labs_examples/objects_classes_methods/labs/methods/Exercise_01.java
+++ src/labs_examples/objects_classes_methods/labs/methods/Exercise_01.java
@@ -9,30 +9,46 @@ public class Exercise_01 {
 
         // please create the methods as instructed below then
         // call each of those methods from here, within the main()
-
+    	multiply(1,2);
+    	divide(10,5);
+    	printJoke();
+    	yearsNumber(3);
+    	
     }
 
 
     // 1) Create a static multiply() method below that takes two int arguments (int a, int b) and
     //    returns the result of a * b
+    public static int multiply(int a, int b) {
+    	return a*b;
+    }
 
 
     // 2) Create a static divide() method below that takes two int arguments (int a, int b) and
     //    returns the result of a / b
-
+    public static int divide(int a, int b) {
+    	return a/b;
+    }
 
     // 3) Create a static void method that will print of joke of your choice to the console
-
+    public static void printJoke() {
+    	System.out.println("Printing Joke");
+    }
 
     // 4) Create a static method that takes in a number in years (int years) as an argument
     //    and returns the number of seconds that number in years represents
-
-
+    public static long yearsNumber(int years) {
+    	long seconds = 60*60*24*years;
+    	return seconds;
+    	
+    }
+    
+    
+    
     // 5) Create a varargs method that will return the length of the varargs array passed in
-
-
-
-
-
+    public static int arrLength(int ...varargs) {
+    	return varargs.length;
+    	
+    }
 
 }
